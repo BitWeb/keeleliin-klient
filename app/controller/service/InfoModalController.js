5cfc4871a86a197c6257513340a3ceb24e4b686f
priit <priit@bitweb.ee>
2015-09-30 14:54:56 +0300

    Teenuse install

--- app/controller/service/InfoModalController.js
+++ app/controller/service/InfoModalController.js
@@ -1,12 +1,45 @@
 define([
     'angularAMD',
-    'ServiceService'
+    'ServiceService',
+    'ResourceTypeService'
 ], function (angularAMD) {
 
-    angularAMD.controller('ServiceInfoModalController', [ '$scope', 'ServiceService','serviceId',
-        function($scope, serviceService, serviceId) {
+    angularAMD.controller('ServiceInfoModalController', [ '$scope', 'ServiceService','ResourceTypeService','serviceId',
+        function($scope, serviceService, resourceTypeService, serviceId) {
+
             serviceService.getService(serviceId, function (err, service) {
                 $scope.service = service;
+
+                resourceTypeService.getResourceTypesList(function (err, types) {
+
+                    $scope.inputTypes = service.serviceInputTypes.map(function (inputType) {
+                        for(i in types){
+                            if(inputType.resourceTypeId == types[i].id){
+                                return types[i].name;
+                            }
+                        }
+                    });
+
+                    $scope.outputTypes = service.serviceOutputTypes.map(function (outputType) {
+                        for(i in types){
+                            if(outputType.resourceTypeId == types[i].id){
+                                return types[i].name;
+                            }
+                        }
+                    });
+                });
+
             });
+
+
+            $scope.getInputTypes = function () {
+
+            };
+
+            $scope.getOutputTypes = function () {
+
+            };
+
+
         }]);
 });
