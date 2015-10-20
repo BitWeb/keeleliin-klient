c9591eaf226d97029192256bc5cea3f3f5ef787b
priit <priit@bitweb.ee>
2015-10-20 14:14:07 +0300

    Docker

--- Dockerfile
+++ Dockerfile
@@ -8,8 +8,11 @@ RUN apt-get update && \
     apt-get -y install python build-essential nodejs
 
 COPY . /
+
+RUN cp -R -u -p app/config/global_dist.js app/config/global.js
+
 RUN npm install
 
-EXPOSE  3000
+EXPOSE  3001
 
 CMD ["npm", "start"]
--- app/config/global_dist.js
+++ app/config/global_dist.js
@@ -4,7 +4,8 @@
 define([], function () {
 
     return {
-        API_URL: 'http://dev.bitweb.ee:3000/api/v1', //http://localhost:8000/api/v1', //API_URL : 'http://dev.bitweb.ee:3000/api/v1',
+        API_URL: 'http://localhost:3000/api/v1',//http://localhost:8000/api/v1', //API_URL : 'http://dev.bitweb.ee:3000/api/v1',
+
         user : {
             roles: ['regular', 'admin']
         },
@@ -14,7 +15,8 @@ define([], function () {
         resource_type: {
             split_types: ['NONE', 'LINE', 'BYTE']
         },
-        hearbeat_interval: 360000
+        workflow_statuses: ['INIT', 'RUNNING', 'FINISHED', 'ERROR', 'CANCELLED'],
+        hearbeat_interval: 3600
     };
 
 });
