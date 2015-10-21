e2af7e4dc1f17da313116a3216b77aa774b6db40
priit <priit@bitweb.ee>
2015-10-21 17:42:21 +0300

    Docker

--- docker_start.sh
+++ docker_start.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 ls -la /config
-cp -R -u -p /src/app/config/global_dist.js /config/config.js
+
+if [ ! -f /config/config.js ]; then
+    cp -R -u -p /src/app/config/global_dist.js /config/config.js
+fi
 
 ln -s /config/config.js /src/app/config/global.js
 
