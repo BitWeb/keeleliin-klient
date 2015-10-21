c7f7b99751b07221b069bbdbf4224776c310b22d
priit <priit@bitweb.ee>
2015-10-21 17:21:47 +0300

    Docker

--- docker_start.sh
+++ docker_start.sh
@@ -3,5 +3,5 @@ cp -R -u -p /src/app/config/global_dist.js /config/config.js
 ln -s /config/config.js /src/app/config/global.js
 
 cd /src
-forever start /src/node_modules/http-server/bin/http-server -p 3001 -c-1
+forever start ./node_modules/http-server/bin/http-server -p 3001 -c-1
 forever list
