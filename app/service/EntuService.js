8cf353cd4b2bb9478388e040b27cc9ad513dceea
priit <priit@bitweb.ee>
2015-12-10 15:12:07 +0200

    definitsioon -> kirjeldus

--- app/service/EntuService.js
+++ app/service/EntuService.js
@@ -3,6 +3,7 @@ define([ 'angularAMD' ], function (angularAMD, resourceTreeMapper) {
     angularAMD.service('EntuService', [ '$log', '$http', 'config', '$modal','UserService',
         function( $log, $http, config, $modal, userService ) {
             var self = this;
+            self.index = 0;
 
             this.getResourcesList = function (params, callback) {
                 $http.get(config.API_URL + '/entu/resource', {params: params}).then(
@@ -62,20 +63,37 @@ define([ 'angularAMD' ], function (angularAMD, resourceTreeMapper) {
 
                 if(node.data && node.data.scope == 'entity'){
 
-                    self.getResourceFilesList(node.id, function (err, data) {
+                    var entityId = node.id.split('-')[0];
+
+                    self.getResourceFilesList(entityId, function (err, data) {
                         var nodes = [];
                         for( var i = 0, length = data.length; i < length; i++){
-                            var item = data[i];
-                            nodes.push({
-                                text: item.name,
-                                id: 'file-' + item.id,
-                                data: {
-                                    scope: item.scope
-                                },
-                                type    : 'text',
-                                children : false
 
-                            });
+                            self.index++;
+
+                            var item = data[i];
+
+                            if( item.scope == 'file' ){
+                                nodes.push({
+                                    text: item.name,
+                                    id: 'file-' + item.id + '-' + self.index,
+                                    data: {
+                                        scope: item.scope
+                                    },
+                                    type    : 'text',
+                                    children : false
+                                });
+                            } else {
+                                nodes.push({
+                                    text: item.name,
+                                    id: item.id + '-' + self.index,
+                                    data: {
+                                        scope: item.scope
+                                    },
+                                    type    : 'default',
+                                    children : true
+                                });
+                            }
                         }
                         callback(null, nodes);
                     });
